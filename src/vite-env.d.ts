d35d460c6197ecd9f0a691327f2b22cf078c5374
TsonkoAlt <matrosmartz@gmail.com>
2023-10-10 15:06:00 -0600

    fix: add new ObjectConstructor.entries definition

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,7 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface ObjectConstructor {
+	entries<T>(o: { [K in keyof T]: T[K] }): Array<[K, T[K]]>
+}
