4fbfba2567f2f185c4c7b454e3dd3064987a8d5a
TsonkoAlt <matrosmartz@gmail.com>
2023-10-19 20:04:29 -0600

    doc: add docs in mediator models

--- src/commands/domain/models/mediator.model.ts
+++ src/commands/domain/models/mediator.model.ts
@@ -8,9 +8,20 @@ export type DispatchArgs = ScreenDispatchArgs | GameDispatchArgs | PrefDispatchA
 export type SubscriberArgs = BoardSubscriberArgs | ModesSubscriberArgs | PrefSubscriberArgs | ScreenSubscribeArgs
 
 export interface IVimMediator {
+	/**
+	 * Dispatches events based on the provided arguments.
+	 * @param args The arguments for dispatching events.
+	 */
 	dispatch(args: DispatchArgs): void
-
+	/**
+	 * Asynchronously loads data.
+	 * @returns A promise that resolves when loading is complete.
+	 */
 	load(): Promise<void>
-
+	/**
+	 * Subscribes to events based on the provided arguments and returns a function to remove the observer.
+	 * @param args The arguments for subscribing to events.
+	 * @returns A function that can be called to remove the observer.
+	 */
 	subscribe(args: SubscriberArgs): RemoveObserver
 }
