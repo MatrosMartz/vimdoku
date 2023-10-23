466dad4daec17ea1abbecb1784e3a61863257919
TsonkoAlt <matrosmartz@gmail.com>
2023-10-22 21:13:32 -0600

    doc: add docs in timer model and service

--- src/sudoku/domain/models/timer.model.ts
+++ src/sudoku/domain/models/timer.model.ts
@@ -1,7 +1,24 @@
 export interface ITimer {
+	/** Get the current time value in seconds. */
 	readonly data: number
+	/**
+	 * Decrements the current time value by 1 second.
+	 * @returns This TimerService instance after decrementing the time value.
+	 */
 	dec(): this
+	/**
+	 * Increments the current time value by 1 second.
+	 * @returns This TimerService instance after incrementing the time value.
+	 */
 	inc(): this
+	/**
+	 * Resets the current time value to 0 seconds.
+	 * @returns This TimerService instance after resetting the time value.
+	 */
 	reset(): this
+	/**
+	 * Converts the current time value to a string representation in the "HH:MM:SS" format.
+	 * @returns A string representing the current time value in "HH:MM:SS" format.
+	 */
 	toString(): string
 }
--- src/sudoku/domain/services/timer.service.ts
+++ src/sudoku/domain/services/timer.service.ts
@@ -1,8 +1,13 @@
 import type { ITimer } from '../models'
 
+/** Represent a TimerBoard Service. */
 export class TimerService implements ITimer {
 	#data
 
+	/**
+	 * Creates an instance of the TimerService class.
+	 * @param data Initial time value in seconds..
+	 */
 	constructor(data: number) {
 		this.#data = data
 	}
@@ -11,6 +16,12 @@ export class TimerService implements ITimer {
 		return this.#data
 	}
 
+	/**
+	 * Creates an instance of TimerService from a string representation of time in the "HH:MM:SS" format.
+	 * @param timerLike A string in the "HH:MM:SS" format representing time.
+	 * @returns A new TimerService instance with the time parsed from the input string.
+	 * @throws {Error} If the input string is not a valid time representation.
+	 */
 	static fromString(timerLike: string) {
 		const [hours, minutes, seconds] = timerLike.split(':').map(Number)
 
@@ -21,14 +32,25 @@ export class TimerService implements ITimer {
 		return new TimerService(hours * 3600 + minutes * 60 + seconds)
 	}
 
+	/**
+	 * Converts the current time value to a string representation in the "HH:MM:SS" format.
+	 * @param data The time value in seconds to be formatted.
+	 * @returns A string representing the time in "HH:MM:SS" format.
+	 */
 	static parseString(data: number) {
 		const seconds = TimerService.#parseUnit(data % 60)
 		const minutes = TimerService.#parseUnit(Math.floor((data % 3600) / 60))
-		const hours = TimerService.#parseUnit(Math.floor(data / 3600), 3)
+		const hours = TimerService.#parseUnit(Math.floor(data / 3600))
 
 		return `${hours}:${minutes}:${seconds}`
 	}
 
+	/**
+	 * Formats a time unit (hours, minutes, or seconds) to have a minimum number of digits.
+	 * @param unit The time unit to be formatted.
+	 * @param digits The minimum number of digits for the formatted unit (default is 2).
+	 * @returns A string representation of the unit with the specified number of digits.
+	 */
 	static #parseUnit(unit: number, digits = 2) {
 		if (unit < 10 * (digits - 1)) return '0'.repeat(digits - 1) + unit
 
