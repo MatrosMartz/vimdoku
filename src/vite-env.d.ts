291423c538189d856f354f6185f7c415a8216f50
TsonkoAlt <matrosmartz@gmail.com>
2023-10-25 21:44:12 -0600

    feat: add new Array.includes definition

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,7 @@ interface ObjectConstructor {
 	entries<O>(o: { [K in keyof O]: O[K] }): Array<[K, O[K]]>
 	keys<O>(o: O): Array<keyof O>
 }
+
+interface Array<T> {
+	includes(searchElement: unknown, fromIndex?: number): searchElement is T
+}
