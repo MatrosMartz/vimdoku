586d229f627988041fa5a4b2a9199aa727fa1c3b
TsonkoAlt <matrosmartz@gmail.com>
2023-11-01 13:22:57 -0600

    fix: preventEvent if press ':' key

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,8 @@
 	import { CommandDialog, PreferencesDialog } from '$screen/infra/components/svelte'
 
 	function keydownHandler(ev: KeyboardEvent) {
-		if (ev.key === ':') {
-			if (mediator.get('screen').dialog.kind === DialogKinds.None) ev.preventDefault()
+		if (ev.key === ':' && mediator.get('screen').dialog.kind !== DialogKinds.Cmd) {
+			ev.preventDefault()
 			mediator.dispatch(ScreenActions.OpenDialog, { kind: DialogKinds.Cmd, opts: { type: CmdDialogTypes.Full } })
 		}
 		if (ev.key === 'Escape') {
