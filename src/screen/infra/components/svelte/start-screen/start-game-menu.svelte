2d2070cfb16cf71e63caeb20b96b5f232e1efdc9
TsonkoAlt <matrosmartz@gmail.com>
2023-11-04 15:59:23 -0600

    feat: add logic for Resume button in StartGameMenu component

--- src/screen/infra/components/svelte/start-screen/start-game-menu.svelte
+++ src/screen/infra/components/svelte/start-screen/start-game-menu.svelte
@@ -1,8 +1,17 @@
 <script>
 	import { Button, ButtonMenu } from '~/share/infra/components/svelte'
+	import { mediator } from '$cmd/infra/services'
+	import { boardSvelte } from '$cmd/infra/stores'
+	import { SudokuActions } from '$sudoku/domain/models'
+
+	$: disabledResume = $boardSvelte == null
+
+	function resumeHandler() {
+		mediator.dispatch(SudokuActions.Resume)
+	}
 </script>
 
 <ButtonMenu>
 	<Button>Start new game.</Button>
-	<Button>Resume game.</Button>
+	<Button disabled={disabledResume} on:click={resumeHandler}>Resume game.</Button>
 </ButtonMenu>
