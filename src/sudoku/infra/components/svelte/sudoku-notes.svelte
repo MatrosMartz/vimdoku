cdc5389a53b9937e3a706b63262e26c1d5348a9e
TsonkoAlt <matrosmartz@gmail.com>
2023-12-03 20:09:22 -0600

    fix: notes state is null

--- src/sudoku/infra/components/svelte/sudoku-notes.svelte
+++ src/sudoku/infra/components/svelte/sudoku-notes.svelte
@@ -5,7 +5,7 @@
 	import { boardState, posState } from '$cmd/infra/stores/svelte'
 	import type { ValidNumbers } from '$sudoku/domain/models'
 
-	$: notes = $boardState![$posState.y][$posState.x].notes
+	$: notes = $boardState![$posState.y][$posState.x].notes ?? Array(9).fill(null)
 
 	$: empty = notes.every(note => note == null)
 
