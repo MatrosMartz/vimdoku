54fe36e7959185b8ae02911996eb057d861bb413
TsonkoAlt <matrosmartz@gmail.com>
2023-12-21 17:29:36 -0600

    feat: add injectable and singleton decorators

--- src/share/utils/index.ts
+++ src/share/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './commons.util'
 export * from './create-array.util'
 export * from './errors.util'
+export * from './injection.util'
 export * from './match.utils'
 export { default as sameStructure } from './same-structure.util'
 export * from './sudoku.utils'
---
+++ src/share/utils/injection.util.ts
@@ -0,0 +1,31 @@
+const create: unique symbol = Symbol('injection')
+
+export type Class<T extends object> = new (...args: any[]) => T
+
+export interface Decorated<T extends object> extends Class<T> {
+	[create](): T
+}
+
+export type Decorator = <const C extends Class<object>>(Target: C) => C
+
+export const injectable = <const C extends Class<object>>(Target: C): C => {
+	return class Injectable extends Target {
+		static [create]() {
+			return new Injectable()
+		}
+	}
+}
+
+export const singleton: Decorator = Target => {
+	return class Singleton extends Target {
+		static readonly #instance: Singleton = new Singleton()
+
+		static [create]() {
+			return Singleton.#instance
+		}
+	}
+}
+
+export function inject<const I extends object>(C: Class<I> | Decorated<I>) {
+	return create in C ? C[create]() : new C()
+}
