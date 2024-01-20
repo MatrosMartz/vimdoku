f81a7f8c40a2b3ec057447812e4940b69b0e1085
TsonkoAlt <matrosmartz@gmail.com>
2024-01-19 19:35:47 -0600

    fix: close and open select mode component

--- src/screen/infra/components/svelte/status-bar/select-mode.svelte
+++ src/screen/infra/components/svelte/status-bar/select-mode.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import { Icon } from '~/share/infra/components/svelte'
 	import { tooltip, type TooltipProps } from '~/share/infra/components/svelte/tooltip'
-	import { posState } from '~/share/infra/stores/svelte'
 	import { med } from '$cmd/infra/services'
 	import { i18nState } from '$i18n/infra/stores/svelte'
 	import { DialogKind, MainScreenKind, ScreenAction } from '$screen/domain/models'
@@ -13,10 +11,8 @@
 
 	$: open = $screenState.dialog.kind === DialogKind.InLn && $screenState.dialog.opts.type === 'modes'
 
-	let openAccordionTimeoutId: ReturnType<typeof setTimeout> | null = null
-
 	$: if (disabled) med.dispatch(ScreenAction.Exit)
-	$: if (open) document.getElementById(`mode-${modeState.data}`)?.focus()
+	$: if (open) setTimeout(() => document.getElementById(`mode-${modeState.data}`)?.focus(), 500)
 
 	$: tooltipProps = {
 		id: 'disabled-mode-reason',
@@ -43,29 +39,22 @@
 	}
 
 	/**
-	 * Clear timeout, focus handler.
+	 * Close if blur input, blur handler.
+	 * @param ev The focus event.
 	 */
-	function focusHandler() {
-		if (openAccordionTimeoutId != null) {
-			clearTimeout(openAccordionTimeoutId)
-			openAccordionTimeoutId = null
-		}
-	}
-
-	/**
-	 * Create timeout, focus-out handler.
-	 */
-	function focusoutHandler() {
-		if (open) openAccordionTimeoutId = setTimeout(() => med.dispatch(ScreenAction.Exit), 150)
+	function closeHandler(ev: FocusEvent): void
+	function closeHandler({ relatedTarget }: FocusEvent) {
+		const relatedIsModeSelector = relatedTarget instanceof HTMLElement && relatedTarget.id === 'mode-selector-header'
+		const relatedIsModeInput = relatedTarget instanceof HTMLInputElement && relatedTarget.name === 'mode'
+		if (!(relatedIsModeInput || relatedIsModeSelector)) med.dispatch(ScreenAction.Exit)
 	}
 
 	/**
 	 * Return focus of the game, Keyup handler.
 	 * @param ev The Keyboard event.
 	 */
-	function keyupHandler(ev: KeyboardEvent): void
-	function keyupHandler({ key }: KeyboardEvent) {
-		if (key === 'Enter') med.dispatch(SudokuAction.Move, { type: 'set', position: posState.data })
+	function keyupHandler(ev: KeyboardEvent) {
+		if (ev.key === 'Enter') med.dispatch(ScreenAction.Exit)
 	}
 </script>
 
@@ -77,30 +66,29 @@
 		aria-controls="mode-selector-panel"
 		aria-disabled={disabled}
 		class="status-icon mode"
-		on:focus={focusHandler}
-		on:focusout={focusoutHandler}
 		on:click={toggleHandler}
 		use:tooltip={disabled ? tooltipProps : null}
 		>{$i18nState.get(`modes-${$modeState}`, $modeState.toUpperCase())}</button
 	>
-	<div id="mode-selector-panel" role="region" aria-labelledby="mode-selector-header">
-		<form method="get">
+	<div class="mode-selector-container">
+		<form id="mode-selector-panel" role="region" aria-labelledby="mode-selector-header" method="get">
 			{#each Object.values(ModeKind) as mode (mode)}
 				<label for="mode-{mode}">
 					<input
 						id="mode-{mode}"
 						name="mode"
+						aria-disabled={!open}
 						type="radio"
+						disabled={!open}
 						tabindex={open ? 0 : -1}
 						value={mode}
 						checked={mode === $modeState}
-						on:focus={focusHandler}
-						on:focusout={focusoutHandler}
 						on:change={modeHandler}
 						on:keyup={keyupHandler}
+						on:blur={closeHandler}
 						use:tooltip={{ id: `mode-${mode}-input-key-describe`, text: `<${MODE_KEYS[mode]}>` }}
 					/>
-					<span>{$i18nState.get(`modes-${mode}`, mode.toUpperCase())}</span><Icon id="check" />
+					<span>{$i18nState.get(`modes-${mode}`, mode.toUpperCase())}</span>
 				</label>
 			{/each}
 		</form>
@@ -110,29 +98,34 @@
 <style>
 	.mode-accordion {
 		position: relative;
-		height: 44px;
+		height: var(--icon-size);
 		color: rgb(var(--value-color));
 	}
 
-	#mode-selector-panel form {
+	.mode-selector-container {
+		height: 100%;
+		clip-path: polygon(0 50%, 100% 50%, 100% -500%, 0 -500%);
+	}
+
+	#mode-selector-panel {
 		position: relative;
 		bottom: 0;
 		z-index: 0;
 		display: grid;
-		grid-template-columns: 1fr;
 		background-color: rgb(var(--status-bar-background));
 		border: 1px solid rgb(var(--tooltip-border));
 		border-bottom: none;
 		border-radius: 8px 8px 0 0;
-		transition: transform 500ms;
+		transition: transform 750ms;
 		transform: translateY(100%);
 	}
 
-	.mode-accordion.open #mode-selector-panel form {
+	.mode-accordion.open #mode-selector-panel {
 		transform: translateY(-100%);
 	}
 
 	input {
+		position: absolute;
 		appearance: none;
 	}
 
@@ -140,9 +133,9 @@
 		position: relative;
 		display: grid;
 		gap: 0.5rem;
-		place-items: center flex-start;
+		place-items: center;
 		width: 100%;
-		height: 44px;
+		height: var(--icon-size);
 		padding-inline: 0.5rem;
 		opacity: 0.6;
 	}
@@ -155,19 +148,6 @@
 	label:has(input:focus) {
 		text-decoration: underline;
 		opacity: 1;
-	}
-
-	label :global(svg) {
-		display: none;
-		color: transparent;
-	}
-
-	label:has(input:checked) :global(svg) {
-		color: rgb(var(--input-border));
-	}
-
-	label:has(input:focus) :global(svg) {
-		color: rgb(var(--key-color));
 	}
 
 	label span {
@@ -194,19 +174,6 @@
 	}
 
 	@media (width >= 480px) {
-		#mode-selector {
-			grid-template-columns: 1fr auto;
-		}
-
-		label {
-			grid-template-columns: subgrid;
-			grid-column: 1 / 3;
-		}
-
-		label :global(svg) {
-			display: initial;
-		}
-
 		.mode {
 			border-radius: 16px 0 0 16px;
 		}
